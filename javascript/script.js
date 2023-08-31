3037888b6ef0d552057cade99d8b43074fe9b1ca
isabelcout <isabelcoutinhors@gmail.com>
2023-08-31 00:20:55 -0300

    exercícios

--- javascript/script.js
+++ javascript/script.js
@@ -12,3 +12,35 @@ if (hora >= 6 && hora < 18) {
 
 console.log(`Está ${dia}`);
 
+// 2 - Exercício
+
+for (let contador = 0; contador <=20; contador +=2){
+    console.log(contador);
+}
+
+// 3 - Exercício
+
+function minhaFuncao() {
+    console.log("Minha primeira função");
+}
+
+minhaFuncao();
+
+// 4 - Exercício
+
+function meuNome(nome) {
+    console.log(`Meu nome é ${nome}`);
+}
+
+meuNome("Isabel Coutinho");
+
+// 5 - Exercício
+
+function meusDados(nome, idade, filme) {
+    console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade e meu filme favorito é ${filme}`);
+}
+
+meusDados("Isabel Coutinho", 21, "Harry Potter!");
+
+// 6 - Exercício
+
